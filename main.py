bc58c6ac530dd601c62b3a01441184e0ca1b694d
pathanon <nontapat_s@cmu.ac.th>
2024-01-14 13:53:03 +0700

    debug random food

--- main.py
+++ main.py
@@ -140,7 +140,7 @@ async def hello(interaction):
 @bot.tree.command(name='random-food',description='Randomly pick me some food menu!')
 async def randfood(interaction):
     random_menu = random.choice(get_menu_all())
-    await interaction.response.send_message(f'ทำอะไรกินดี...งั้นลอง {random_menu} ไหมครับ\n')
+    # await interaction.response.send_message(f'ทำอะไรกินดี...งั้นลอง {random_menu} ไหมครับ\n')
     await interaction.response.send_message(get_info_byname(random_menu))
 
 
